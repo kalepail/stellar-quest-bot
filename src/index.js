aae8c1ce84ad52ff9f891f7d2485c8ffda4705f3
Tyler van der Hoeven <hi@tyvdh.com>
2020-11-11 18:19:04 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,108 @@
+if (process.env.NODE_ENV === 'development')
+  require('dotenv').config()
+
+const fetch = require('node-fetch')
+const { Client } = require('discord.js')
+const client = new Client({partials: [
+  'MESSAGE',
+  'CHANNEL',
+  'USER',
+  'REACTION',
+  'GUILD_MEMBER'
+]})
+
+client.on('raw', async (packet) => {
+  const {t: type, d: data} = packet
+  // console.log(packet)
+
+  const channel = await client.channels.fetch('775930950034260008')
+
+  switch (type) {
+    case 'READY':
+      await channel.messages.fetch().then((messages) => messages.map(dealWithMessage))
+      const bootMessage = await channel.send(`${process.env.NODE_ENV} system booted`)
+
+      setTimeout(() => bootMessage.delete(), 10000)
+    break
+
+    case 'MESSAGE_REACTION_ADD':
+      let message = await channel.messages.fetch(data.message_id)
+
+      if (!message.author.bot) return
+
+      if (
+        data.emoji.name !== '👍'
+        && data.emoji.name !== '👎'
+      ) {
+        await message.reactions.cache.get(data.emoji.name).remove()
+        return
+      }
+
+      await Promise.all(message.reactions.cache.map((reaction) => {
+        if (reaction.emoji.name !== data.emoji.name)
+          return reaction.users.remove(data.user_id)
+      }))
+
+      message = await message.fetch(true)
+
+      await dealWithMessage(message)
+    break
+
+    default:
+    return
+  }
+})
+
+client.login(process.env.DISCORD_BOT_TOKEN)
+
+async function dealWithMessage(message) {
+  const upvotes = message.reactions.cache.filter((reaction) => reaction.emoji.name === '👍').first()
+  const downvotes = message.reactions.cache.filter((reaction) => reaction.emoji.name === '👎').first()
+
+  const baseUrl = process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:8787' : 'https://api-quest.stellar.buzz'
+
+  let [ id,,badge ] = message.content.split('\n')
+      id = id.replace(/\s/g, '').split(':')[1]
+      badge = badge.replace(/\s/g, '').split(':')[1]
+
+  const series = parseInt(badge.match(/\d{2}/g)[0])
+
+  const body = {
+    id,
+    token: process.env.GROOT_KEY,
+  }
+
+  if (upvotes && upvotes.count >= 2) {
+    console.log('accept')
+
+    await fetch(`${baseUrl}/user/submit?series=${series}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        ...body,
+        verified: true
+      })
+    })
+
+    message.delete()
+  }
+
+  if (downvotes && downvotes.count >= 2) {
+    console.log('reject')
+
+    await fetch(`${baseUrl}/user/submit?series=${series}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        ...body,
+        verified: false
+      })
+    })
+
+    message.delete()
+  }
+}
