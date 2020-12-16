3ca410083176386856488c8efb20956563fdad71
Tyler van der Hoeven <hi@tyvdh.com>
2020-12-15 21:57:23 -0500

    Better support for multiple series

--- fraud-squad.js
+++ fraud-squad.js
@@ -1,5 +1,4 @@
 const isDev = process.env.NODE_ENV === 'development'
-const args = process.argv.slice(2)
 
 if (isDev)
   require('dotenv').config()
@@ -30,78 +29,77 @@ call()
 async function call() {
   await client.login(process.env.DISCORD_BOT_TOKEN)
 
-  return fetch(`https://api-quest.stellar.buzz/utils/pending?series=${args[0]}&token=${process.env.GROOT_KEY}`)
-  .then(async (res) => {
-    const body = await res.json()
+  const pendingUsers = []
+  const fraudChannel = await client.channels.fetch('775930950034260008', true, true)
+  const verifyChannel = await client.channels.fetch('764096940450250763', true, true)
 
-    if (res.ok) {
-      if (args[0] === 1) {
-        const verifyChannel = await client.channels.fetch('764096940450250763', true, true)
+  let fcFetched
+  let vcFetched
 
-        let vcFetched
+  await Bluebird.mapSeries([
+    1,
+    2
+  ], (i) =>
+    fetch(`https://api-quest.stellar.buzz/utils/pending?series=${i}&token=${process.env.GROOT_KEY}`)
+    .then(async (res) => {
+      if (res.ok)
+        pendingUsers.push(...await res.json())
+    })
+  )
 
-        do {
-          vcFetched = await verifyChannel.messages.fetch({limit: 100}, true, true)
+  do {
+    fcFetched = await fraudChannel.messages.fetch({limit: 100}, true, true)
 
-          vcFetched = vcFetched.filter((message) =>
-            moment.utc(message.createdTimestamp, 'x').isBefore(moment.utc().subtract(1, 'week'))
-          )
+    fcFetched = fcFetched.filter((message) => {
+      if (message.author.username.indexOf('→') === -1)
+        return true
 
-          // await Bluebird.mapSeries((vcFetched), ([id, message]) => {
-          //   console.log('deleted', id)
-          //   return message.delete()
-          // })
+      const id = message.author.username.split('→')[1].trim()
+      const index = findIndex(pendingUsers, {id})
 
-          await verifyChannel.bulkDelete(vcFetched)
-        } while(vcFetched.size >= 1)
+      if (index > -1) {
+        pendingUsers.splice(index, 1)
+        return false
       }
+    })
 
-      const fraudChannel = await client.channels.fetch('775930950034260008', true, true)
+    await fraudChannel.bulkDelete(fcFetched)
+  } while(fcFetched.size >= 1)
 
-      let fcFetched
-
-      do {
-        fcFetched = await fraudChannel.messages.fetch({limit: 100}, true, true)
-
-        fcFetched = fcFetched.filter((message) => {
-          if (message.author.username.indexOf('→') === -1)
-            return true
-
-          const id = message.author.username.split('→')[1].trim()
-          const index = findIndex(body, {id})
-
-          if (index > -1) {
-            body.splice(index, 1)
-            return false
-          }
-        })
-
-        await fraudChannel.bulkDelete(fcFetched)
-      } while(fcFetched.size >= 1)
-
-      await new Bluebird.mapSeries(body, (user) => {
-        const webhookOptions = {
-          username: user.id
-        }
-
-        if (user.authHash) {
-          const authHash = JSON.parse(Buffer.from(user.authHash, 'base64').toString())
-
-          if (authHash.avatar)
-            webhookOptions.avatarURL = `https://cdn.discordapp.com/avatars/${session.aud}/${authHash.avatar}.png`
-          else
-            webhookOptions.avatarURL = `https://cdn.discordapp.com/embed/avatars/${authHash.discriminator % 5}.png`
-
-          webhookOptions.username = `${authHash.username} → ${authHash.id}`
-        }
-
-        return hook.send(`<${isDev ? 'http://localhost:3333' : 'https://quest.stellar.org'}/users/inspect/${user.id}>`, webhookOptions)
-      })
-
-      return `Added ${body.length} pending users`
+  await new Bluebird.mapSeries(pendingUsers, (user) => {
+    const webhookOptions = {
+      username: user.id
     }
 
-    else
-      throw body
+    if (user.authHash) {
+      const authHash = JSON.parse(Buffer.from(user.authHash, 'base64').toString())
+
+      if (authHash.avatar)
+        webhookOptions.avatarURL = `https://cdn.discordapp.com/avatars/${user.id}/${authHash.avatar}.png`
+      else
+        webhookOptions.avatarURL = `https://cdn.discordapp.com/embed/avatars/${authHash.discriminator % 5}.png`
+
+      webhookOptions.username = `${authHash.username} → ${authHash.id}`
+    }
+
+    return hook.send(`<${isDev ? 'http://localhost:3333' : 'https://quest.stellar.org'}/users/inspect/${user.id}>`, webhookOptions)
   })
+
+  do {
+    vcFetched = await verifyChannel.messages.fetch({limit: 100}, true, true)
+
+    vcFetched = vcFetched.filter((message) =>
+      moment.utc(message.createdTimestamp, 'x').isBefore(moment.utc().subtract(1, 'week'))
+    )
+
+    // Only needed if messages are older than two weeks
+    // await Bluebird.mapSeries((vcFetched), ([id, message]) => {
+    //   console.log('deleted', id)
+    //   return message.delete()
+    // })
+
+    await verifyChannel.bulkDelete(vcFetched)
+  } while(vcFetched.size >= 1)
+
+  return `Added ${pendingUsers.length} pending users`
 }
