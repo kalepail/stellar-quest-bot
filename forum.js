ddc0cedc46f400112269faac5a1e5eb73fc62f22
Joscha Rohmann <joscha@kanaye.net>
2022-11-17 10:01:59 +0100

    Add NEW flag and change purpose of OPEN flag

--- forum.js
+++ forum.js
@@ -2,6 +2,17 @@ const { Client, ApplicationCommandType, ApplicationCommandOptionType, MessageFla
 
 module.exports.commandName = 'post'
 
+const {
+  FORUM_CHANNEL_HELP,
+  LUMENAUT_ROLE_ID,
+  HELP_TAG_BUG,
+  HELP_TAG_DUPLICATE,
+  HELP_TAG_FLAGGED,
+  HELP_TAG_NEW,
+  HELP_TAG_OPEN,
+  HELP_TAG_SOLVED
+} = process.env
+
 module.exports.setup = async function setupForum(client) {
   client.on('messageCreate', handleMessage)
     .on('threadCreate', handlePost)
@@ -39,10 +50,14 @@ module.exports.setup = async function setupForum(client) {
         type: ApplicationCommandOptionType.Subcommand,
         name: 'bug',
         description: 'Toggle the solved tag.'
-      },{
+      }, {
         type: ApplicationCommandOptionType.Subcommand,
         name: 'flag',
         description: 'Toggle the flagged tag.'
+      }, {
+        type: ApplicationCommandOptionType.Subcommand,
+        name: 'open',
+        description: 'Tag this post as open and remove the solved tag is set.'
       }]
     })
   }
@@ -55,14 +70,15 @@ module.exports.setup = async function setupForum(client) {
 async function handleMessage(message) {
   const channel = message.channel
   if (!channel.isThread
-    || channel.parentId != process.env.FORUM_CHANNEL_HELP)
+    || channel.parentId != FORUM_CHANNEL_HELP)
     return
-  if (message.member?.roles?.cache.has(process.env.LUMENAUT_ROLE_ID)
-    && channel.appliedTags.includes(process.env.HELP_TAG_OPEN)) {
+  if (message.member?.roles?.cache.has(LUMENAUT_ROLE_ID)
+    && channel.appliedTags.includes(HELP_TAG_NEW)) {
+    const tags = channel.appliedTags
+    removeTag(tags, HELP_TAG_NEW)
+    addTag(tags, HELP_TAG_OPEN)
     await channel.setAppliedTags(
-      channel.appliedTags.filter(
-        tag => tag != process.env.HELP_TAG_OPEN
-      ),
+      tags,
       'Lumenaut answered post.'
     )
   }
@@ -82,62 +98,76 @@ const ensureTag = (tags, tag) => {
     tags.push(tag)
 }
 
+const removeTag = (tags, tag) => {
+  let idx;
+  if ((idx = tags.indexOf(tag)) >= 0)
+    tags.splice(idx, 1)
+}
+
 async function handleInteraction(interaction) {
   if (!interaction.channel.isThread
     || interaction.channel.parentId != process.env.FORUM_CHANNEL_HELP) {
-      return interaction.reply({
-        flags: MessageFlags.Ephemeral,
-        content: ':x: Invalid Channel'
-      })
-    }
-
-    const actor = `${interaction.member.user.username}#${interaction.member.user.tag}`
-    const tags = interaction.channel.appliedTags
-
-    // we need to reply before doing anything
-    // otherwise we'd reopen the post by writing to it ...
-    await interaction.reply({
+    return interaction.reply({
       flags: MessageFlags.Ephemeral,
-      content: ':white_check_mark: Processing.'
-
+      content: ':x: Invalid Channel'
     })
-    switch (interaction.options.getSubcommand(true)) {
-      case 'close':
-          ensureTag(tags, process.env.HELP_TAG_SOLVED)
-          await interaction.channel.setAppliedTags(tags, `Closed by ${actor}`)
-          await interaction.channel.setArchived(true)
-        break;
-      case 'solve':
-          toggle(tags, process.env.HELP_TAG_SOLVED)
-          await interaction.channel.setAppliedTags(tags, `Solved tag toggled by ${actor}`)
-        break;
-      case 'lock':
-        ensureTag(tags, process.env.HELP_TAG_SOLVED)
-        await interaction.channel.setAppliedTags(tags, `Locked by ${actor}`)
-        await interaction.channel.setLocked(true)
-        await interaction.channel.setArchived(true)
-        break;
-      case 'duplicate':
-          toggle(tags, process.env.HELP_TAG_DUPLICATE)
-          await interaction.channel.setAppliedTags(tags, `Duplicate tag toggled by ${actor}`)
-        break;
-      case 'bug':
-          toggle(tags, process.env.HELP_TAG_BUG)
-          await interaction.channel.setAppliedTags(tags, `Bug tag toggled by ${actor}`)
-        break;
-      case 'flag':
-          toggle(tags, process.env.HELP_TAG_FLAGGED)
-          await interaction.channel.setAppliedTags(tags, `Flagged tag toggled by ${actor}`)
-        break;
-    }
+  }
+
+  const actor = `${interaction.member.user.username}#${interaction.member.user.tag}`
+  const tags = interaction.channel.appliedTags
+
+  // we need to reply before doing anything
+  // otherwise we'd reopen the post by writing to it ...
+  await interaction.reply({
+    flags: MessageFlags.Ephemeral,
+    content: ':white_check_mark: Processing.'
+
+  })
+  switch (interaction.options.getSubcommand(true)) {
+    case 'close':
+      removeTag(tags, HELP_TAG_OPEN)
+      ensureTag(tags, HELP_TAG_SOLVED)
+      await interaction.channel.setAppliedTags(tags, `Closed by ${actor}`)
+      await interaction.channel.setArchived(true)
+      break
+    case 'solve':
+      removeTag(tags, HELP_TAG_OPEN)
+      toggle(tags, HELP_TAG_SOLVED)
+      await interaction.channel.setAppliedTags(tags, `Solved tag toggled by ${actor}`)
+      break
+    case 'lock':
+      removeTag(tags, HELP_TAG_OPEN)
+      ensureTag(tags, HELP_TAG_SOLVED)
+      await interaction.channel.setAppliedTags(tags, `Locked by ${actor}`)
+      await interaction.channel.setLocked(true)
+      await interaction.channel.setArchived(true)
+      break
+    case 'duplicate':
+      toggle(tags, HELP_TAG_DUPLICATE)
+      await interaction.channel.setAppliedTags(tags, `Duplicate tag toggled by ${actor}`)
+      break
+    case 'bug':
+      toggle(tags, HELP_TAG_BUG)
+      await interaction.channel.setAppliedTags(tags, `Bug tag toggled by ${actor}`)
+      break
+    case 'flag':
+      toggle(tags, HELP_TAG_FLAGGED)
+      await interaction.channel.setAppliedTags(tags, `Flagged tag toggled by ${actor}`)
+      break
+    case 'open':
+      removeTag(tags, HELP_TAG_SOLVED)
+      ensureTag(tags, HELP_TAG_OPEN)
+      await interaction.channel.setAppliedTags(tags, `Post opened by ${actor}`)
+      break
+  }
 }
 
 /**
- * Sets the open tag to a new forum post
+ * Sets the new tag to a new forum post
  */
 async function handlePost(thread) {
-  if (thread.parentId != process.env.FORUM_CHANNEL_HELP
+  if (thread.parentId != FORUM_CHANNEL_HELP
     || thread.appliedTags.length)
     return
-  await thread.setAppliedTags([process.env.HELP_TAG_OPEN], 'Help post created.')
+  await thread.setAppliedTags([HELP_TAG_NEW], 'Help post created.')
 }
